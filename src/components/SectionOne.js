e6c2ad3beae491043c730b95fd79228e40eef45c
Abhinav <FASTlap@2711>
2021-05-31 12:32:13 +0530

    fear: New Changes

--- src/components/SectionOne.js
+++ src/components/SectionOne.js
@@ -5,8 +5,10 @@ import img1 from "../asset/Image/resize2.jpg";
 import leftImg from "../asset/Image/left.png";
 import rightImg from "../asset/Image/right.png";
 import { Parallax, Background } from "react-parallax";
+import tileImg1 from "../asset/Image/resize8.jpg";
+import tileImg2 from "../asset/Image/resize9.jpg";
 import ImageSlider from './ImageSlider';
-import { Container, Form, Row, Button } from 'react-bootstrap';
+import { Container, Form, Row, Button, Col } from 'react-bootstrap';
 import VideoContainer from './VideoContainer';
 import PhotoGrid from "./PhotoGrid";
 import { wedPhotos } from "../asset/Image/grid-photos";
@@ -29,6 +31,38 @@ export default function SectionOne() {
                     <VideoContainer></VideoContainer>
                 </div>
             </div>
+            <Container fluid>
+            <Row className="margin-0-padding-0">
+                <Col md={6} className="margin-0-padding-0">
+                    {/* <img src={check}></img> */}
+                    <div className="text-tile">
+                        <div className="margin-auto media-padding-top-100">
+                            <h2 className="text-tile-heading"><span>We focus on prompts for natural expressions, not poses.&nbsp;</span></h2>
+                            <p className="text-tile-sub-heading">"Kiss like you&nbsp;<span>haven't&nbsp;</span><span>seen each other in years."</span></p>
+                            <p className="text-tile-sub-heading">"Say the funniest word you know."</p>
+                        </div>
+                    </div>
+                </Col>
+                <Col md={6} className="margin-0-padding-0">
+                    <img src={tileImg1} style={{ width: "100%" }}></img>
+                </Col>
+            </Row>
+            <Row className="margin-0-padding-0">
+                <Col md={6} className="margin-0-padding-0">
+                    <img src={tileImg2} style={{ width: "100%" }}></img>
+                </Col>
+                <Col md={6} className="margin-0-padding-0">
+                    {/* <img src={check}></img> */}
+                    <div className="text-tile">
+                        <div className="margin-auto media-padding-top-100">
+                            <h2 className="text-tile-heading"><span>Capturing an experience, not just another photograph.&nbsp;</span></h2>
+                            {/* <p className="text-tile-sub-heading">"Kiss like you&nbsp;<span>haven't&nbsp;</span><span>seen each other in years."</span></p> */}
+                            <p className="text-tile-sub-heading">"Like getting together with old friends, we'll have plenty of laughs along the way."</p>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
             <Container fluid className="d-flex">
                 <Row className="recent-photograph">
                     {/* <img src={leftImg} /> */}
@@ -38,6 +72,9 @@ export default function SectionOne() {
             </Container>
             <PhotoGrid imageArr={wedPhotos}></PhotoGrid>
             {/* <ImageSlider></ImageSlider> */}
+            <Parallax bgImage={check} strength={300}>
+                <img src={check} style={{ visibility: "hidden", width: "100%" }} />
+            </Parallax>
             <Container fluid className="d-flex">
                 <Row className="recent-photograph">
                     Wedding Photography
